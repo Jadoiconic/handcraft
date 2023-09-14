a585a394cae07833446d2ac693bff1ff9a7776bf
Nizeyimana Jean de Dieu <86121130+Jadoiconic@users.noreply.github.com>
2023-09-14 15:45:10 +0200

    intial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 function App() {
+  const [zoom, setZoom] = useState(0.5);
+  const [rotation, setRotation] = useState(0);
+  const [xAxis, setXAxis] = useState(0);
+  const [yAxis, setYAxis] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="w-screen h-[80vh] flex justify-center items-center">
+        <motion.h1
+          className=" bg-slate-400 rounded px-10 py-5"
+          animate={{
+            x: xAxis,
+            y: yAxis,
+            scale: zoom,
+            rotate: rotation,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Hello How are you!
+        </motion.h1>
+      </div>
+      <div className="px-10">
+        <button
+          onClick={() => setXAxis(xAxis - 5)}
+          className="px-4 py-2 bg-indigo-400 text-white rounded mr-2"
+        >
+          Left
+        </button>
+        <button
+          onClick={() => setYAxis(yAxis - 5)}
+          className="px-4 py-2 bg-indigo-400 text-white rounded mr-2"
+        >
+          Top
+        </button>
+        <button
+          onClick={() => setXAxis(xAxis + 5)}
+          className="px-4 py-2 bg-indigo-400 text-white rounded mr-2"
+        >
+          Right
+        </button>
+        <button
+          onClick={() => setYAxis(yAxis + 5)}
+          className="px-4 py-2 bg-indigo-400 text-white rounded mr-2"
+        >
+          Bottom
+        </button>
+        <button
+          onClick={() => setRotation(rotation + 5)}
+          className="px-4 py-2 bg-indigo-400 text-white rounded mr-2"
+        >
+          Rotate
+        </button>
+        <button
+          onClick={() => setZoom(zoom + 0.1)}
+          className="px-4 py-2 bg-indigo-400 text-white rounded mr-2"
+        >
+          Zoom In
+        </button>
+        <button
+          onClick={() => setZoom(zoom - 0.1)}
+          className="px-4 py-2 bg-indigo-400 text-white rounded mr-2"
+        >
+          Zoom Out
+        </button>
+      </div>
+    </>
   );
 }
 
