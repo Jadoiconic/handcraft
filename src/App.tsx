518972f89fd2a3f496bd9f4b17a1b3440ca1259a
Nizeyimana Jean de Dieu <86121130+Jadoiconic@users.noreply.github.com>
2023-09-21 01:33:04 +0200

    adding footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,15 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import NavBar from "./components/header/NavBar";
+import FooterSection from "./components/footer/FooterSection";
 function App() {
-  const [zoom, setZoom] = useState(0.5);
-  const [rotation, setRotation] = useState(0);
-  const [xAxis, setXAxis] = useState(0);
-  const [yAxis, setYAxis] = useState(0);
   return (
     <>
     <NavBar />
-      <div className="w-screen h-[80vh] flex justify-center items-center">
-        <motion.h1
-          className=" bg-slate-400 rounded px-10 py-5"
-          animate={{
-            x: xAxis,
-            y: yAxis,
-            scale: zoom,
-            rotate: rotation, transition:[0,1],
-          }}
-          drag
-           whileDrag={{backgroundColor:'teal'}}
-        >
-          Hello How are you!
-        </motion.h1>
+      <div className="w-screen min-h-[60vh] flex justify-center items-center">
+        
       </div>
-      <div className="px-10">
-        <button
-          onClick={() => setXAxis(xAxis - 5)}
-          className="px-4 py-2 bg-indigo-400 text-white rounded mr-2"
-        >
-          Left
-        </button>
-        <button
-          onClick={() => setYAxis(yAxis - 5)}
-          className="px-4 py-2 bg-indigo-400 text-white rounded mr-2"
-        >
-          Top
-        </button>
-        <button
-          onClick={() => setXAxis(xAxis + 5)}
-          className="px-4 py-2 bg-indigo-400 text-white rounded mr-2"
-        >
-          Right
-        </button>
-        <button
-          onClick={() => setYAxis(yAxis + 5)}
-          className="px-4 py-2 bg-indigo-400 text-white rounded mr-2"
-        >
-          Bottom
-        </button>
-        <button
-          onClick={() => setRotation(rotation + 5)}
-          className="px-4 py-2 bg-indigo-400 text-white rounded mr-2"
-        >
-          Rotate
-        </button>
-        <button
-          onClick={() => setZoom(zoom + 0.1)}
-          className="px-4 py-2 bg-indigo-400 text-white rounded mr-2"
-        >
-          Zoom In
-        </button>
-        <button
-          onClick={() => setZoom(zoom - 0.1)}
-          className="px-4 py-2 bg-indigo-400 text-white rounded mr-2"
-        >
-          Zoom Out
-        </button>
-      </div>
+      <FooterSection />
     </>
   );
 }
--- src/components/footer/FooterSection.tsx
+++ src/components/footer/FooterSection.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const FooterSection = () => {
   return (
-    <div>FooterSection</div>
-  )
-}
+    <>
+    <div className="py-2 px-8 bg-slate-800 md:flex md:justify-between">
+      <div>
+        <h1 className="text-[#00f900] border-b border-b-slate-500 text-xl font-bold">Get to Know Us</h1>
+        <ul className="py-2">
+          <li className="py-2 text-slate-200">Blog</li>
+          <li className="py-2 text-slate-200">About Handcraft</li>
+          <li className="py-2 text-slate-200">Investors Relatios</li>
+        </ul>
+      </div>
+      <div>
+        <h1 className="text-[#00f900] border-b border-b-slate-500 text-xl font-bold">Our Misstion</h1>
+      </div>
+      <div>
+        <h1 className="text-[#00f900] border-b border-b-slate-500 text-xl font-bold">Our Vision</h1>
+      </div>
+      <div>
+        <h1 className="text-red-600 border-b border-b-slate-500 text-xl font-bold">Subscribe</h1>
+        <h2 className="text-slate-300 text-lg py-4 outline-none">for  more Information</h2>
+        <div className="flex rounded overflow-hidden bg-red-600">
+          <input type="text" className=" p-2 rounded  border border-red-600 border-1" />
+          <button className=" p-2">Subscirbe</button>
+        </div>
+      </div>
+    </div>
+    <div className="py-5 px-8 text-center bg-slate-900 w-full">
+      <h1 className="text-white">&copy; Copyright Handcraft</h1>
+    </div>
+    </>
+  );
+};
 
-export default FooterSection
+export default FooterSection;
