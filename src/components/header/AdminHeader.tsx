3a1b2d67691f63d08baded1f60c728c64663739d
NIZEYIMANA Jean de Dieu <86121130+Jadoiconic@users.noreply.github.com>
2023-10-07 17:51:53 +0200

    updating admin menus

--- src/components/header/AdminHeader.tsx
+++ src/components/header/AdminHeader.tsx
@@ -3,13 +3,17 @@ import React from "react";
 const AdminHeader = () => {
   return (
     <>
-      <div className="w-full h-10 bg-white py-7 shadow-lg sm:px-2 md:px-8 flex sticky top-0 z-10 justify-between items-center">
-        <div className="text-xl">HandCraft</div>
+      <div className="w-full h-10 bg-white py-7 shadow-lg px-2 sm:px-8 md:px-8 flex sticky top-0 z-10 justify-between items-center">
+        <div className="text-xl ">HandCraft</div>
         <div>
           <nav className="text-xl">
-            <div className="w-10 h-10 bg-indigo-400 rounded-full text-white text-lg font-bold justify-center items-center flex"> T</div>
+            <div className="hidden w-10 h-10 bg-indigo-400 rounded-full text-white text-lg font-bold justify-center items-center md:flex"> T</div>
+            
           </nav>
         </div>
+      </div>
+      <div className="bg-slate-700 h-screen w-5/6">
+        <h1 className="f-1">Hell Navigation</h1>
       </div>
     </>
   );
--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -9,30 +9,30 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <>
       <AdminHeader />
-      <div className="w-full h-screen flex fixed">
-        <div className="shadow-md h-[95vh] w-1/6 bg-slate-900 stiky top-20">
-          <nav className="">
+      <div className="hidden w-full h-screen md:flex lg:flex fixed">
+        <div className="shadow-md h-[95vh] w-1/6 bg-slate-700 stiky top-20">
+          <nav className="py-4">
             <ul>
-              <li className="hover:bg-slate-700 px-4 py-1 flex items-center">
-                <div className="w-5 h-5 rounded-full bg-red-600" />
-                <NavLink to="" className="text-white text-lg mx-2">
+              <li className="hover:bg-slate-400 px-4 py-1 flex items-center">
+                <div className="w-2 h-2 rounded-full bg-red-300" />
+                <NavLink to="/dashboard" className="text-white text-lg mx-2">
                   Dashboard
                 </NavLink>
               </li>
-              <li className="hover:bg-slate-700 px-4 py-1 flex items-center">
-                <div className="w-5 h-5 rounded-full bg-red-600" />
+              <li className="hover:bg-slate-400 px-4 py-1 flex items-center">
+                <div className="w-2 h-2 rounded-full bg-red-300" />
                 <NavLink to="" className="text-white text-lg mx-2">
                   New Product
                 </NavLink>
               </li>
-              <li className="hover:bg-slate-700 px-4 py-1 flex items-center">
-                <div className="w-5 h-5 rounded-full bg-red-600" />
+              <li className="hover:bg-slate-400 px-4 py-1 flex items-center">
+                <div className="w-2 h-2 rounded-full bg-red-300" />
                 <NavLink to="" className="text-white text-lg mx-2">
                   Pending Orders
                 </NavLink>
               </li>
-              <li className="hover:bg-slate-700 px-4 py-1 flex items-center">
-                <div className="w-5 h-5 rounded-full bg-red-600" />
+              <li className="hover:bg-slate-400 px-4 py-1 flex items-center">
+                <div className="w-2 h-2 rounded-full bg-red-300" />
                 <NavLink to="" className="text-white text-lg mx-2">
                   Reports
                 </NavLink>
@@ -40,14 +40,15 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
             </ul>
           </nav>
         </div>
-        <div className="py-2 w-full">
-          <div className="h-3/4">
+        <div className="py-2 w-full h-auto bg-slate-400 overflow-scroll">
+          <div className="min-h-screen">
           {children}
           </div>
-          <div className="flex items-center justify-between bg-slate-700 h-20 px-4 w-full bottom-0">
+          <div className="flex items-center justify-between bg-slate-900 h-20 px-4 w-full bottom-0">
             <div className="text-white py-2 ">&copy;Copyright</div>
             <div className="text-white py-2 ">Powered By Amina</div>
           </div>
+          <div className="py-4"></div>
         </div>
       </div>
     </>
